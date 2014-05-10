2e14fbf8876d9b8deab1ddc9ebb9e4abb43e51fc
ls0 <liddiasue@gmail.com>
2014-05-10 17:34:14 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -11,18 +11,18 @@ hpc <- read.csv2.sql("household_power_consumption.txt",
 DateTime <- as.character(paste0(hpc$Date, " ", hpc$Time))
 d <- cbind(DateTime, select(hpc, -Time, -Date))
  ## Plot "plot4.png".
-    # Global_active_power
+  # Global_active_power
 library(grDevices)
 png(filename="plot4.png",width=480,height=480, type = c("windows"))
 par(mfrow=c(2,2))
 plot(d$Global_active_power~d$DateTime, xaxt="n", type = "l", ylab = "Global Active Power (killowatts)", xlab = "")
 axis(1, at=c(1, 1440, 2880), lab=c("Thu","Fri", "Sat"))
 lines(d$DateTime, d$Global_active_power)
-    # Voltage
+  # Voltage
 plot(d$Voltage~d$DateTime, xaxt="n", type = "l", ylab = "Voltage", xlab = "datetime")
 axis(1, at=c(1, 1440, 2880), lab=c("Thu","Fri", "Sat"))
 lines(d$DateTime, d$Voltage, col = "black") 
-    # Sub_metering
+  # Sub_metering
 plot(as.numeric(d$Sub_metering_1)~d$DateTime, xaxt="n", type = "l", ylab = "Energy sub metering", xlab = "")
 axis(1, at=c(1, 1440, 2880), lab=c("Thu","Fri", "Sat"))
 lines(as.numeric(d$DateTime), d$Sub_metering_1, col = "black")
@@ -30,7 +30,7 @@ lines(as.numeric(d$DateTime), d$Sub_metering_2, col = "red")
 lines(as.numeric(d$DateTime), d$Sub_metering_3, col = "blue")
 legend(1335, 39, c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        col=c("black","red","blue"), cex=0.8, lty=c(1,1), box.col = "white")
-    # Global_reactive_power
+  # Global_reactive_power
 plot(d$Global_reactive_power~d$DateTime, xaxt="n", type = "l", ylab = "Global_reactive_power", xlab = "datetime")
 axis(1, at=c(1, 1390, 2880), lab=c("Thu","Fri", "Sat"))
 lines(d$DateTime, d$Global_reactive_power, col = "black")
